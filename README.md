63aa579bacb6623f3aedda561c3c165fc68f1a66
aswinzz <iit2016106@iiita.ac.in>
2017-07-14 21:01:27 +0530

    add files

---
+++ README.md
@@ -0,0 +1,19 @@
+# Details
+This is a webcrawler built using python.You just have to give the url and the element and class of the website where the content is present.
+
+
+# Installation
+
+```sh
+Install Python
+$ pip install bs4
+$ python webcrawler.py
+enter the url
+enter the element(eg: a,div) and the class 
+enjoy :smile: 
+```
+
+# Screens
+
+![scr1](scr1.png)
+![scr2](scr2.png)
---
+++ webcrawler.py
@@ -0,0 +1,19 @@
+import requests
+from bs4 import BeautifulSoup
+
+def trade_spider(url,element,classname):
+	source = requests.get(url)
+	text = source.text
+	soup = BeautifulSoup(text,"html.parser")
+	for link in soup.findAll(element,{'class':classname}):
+		content=link.string
+		linkid = link.get('href')
+		print("Title:")
+		print(content+"\n")
+		print("Link:")
+		print(linkid+"\n")
+
+url=input("Enter the url of the site to be crawled : ")
+element=input("enter the element from which data is needed (like a,div etc) : ")
+classname=input("enter the class name : ")
+trade_spider(url,element,classname)
