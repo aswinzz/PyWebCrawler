4927a2eb38f767376dab128ae4490293ca4cd929
Aswin VB <iit2016106@iiita.ac.in>
2017-07-18 00:41:17 +0530

    add save to crawler.txt feature

--- webcrawler.py
+++ webcrawler.py
@@ -5,11 +5,18 @@ def trade_spider(url,element,classname):
 	source = requests.get(url)
 	text = source.text
 	soup = BeautifulSoup(text,"html.parser")
+	text=""
 	for link in soup.findAll(element,{'class':classname}):
 		content=link.string
 		linkid = link.get('href')
-		print(content+"\n")
-		print(linkid+"\n")
+		print("Title:")
+		print(content,"\n")
+		print("Link:")
+		print(linkid,"\n")
+		text=text+content+"\n"+linkid+"\n"
+	fw = open('crawler.txt', 'w')
+	fw.write(text)
+	print('\nResult saved to file crawler.txt')
 
 url=input("Enter the url of the site to be crawled : ")
 element=input("enter the element from which data is needed (like a,div etc) : ")
